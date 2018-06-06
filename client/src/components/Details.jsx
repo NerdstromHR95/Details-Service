bb25bc6e7cc178c61532f6b7b0730a8100f288bd
Dan Olson <danielolsonah.school@gmail.com>
2018-06-05 17:16:16 -0700

    API working, react component coming together

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -6,7 +6,9 @@ class Details extends React.Component {
   constructor() {
     super();
     this.state = {
-      product: '',
+      product: {},
+      colors: [],
+      sizes: [{}, {}, {}],
     };
   }
 
@@ -18,15 +20,57 @@ class Details extends React.Component {
         });
       })
       .catch(function(err) {
-        console.log('An error occurred: ', err)
+        console.log('An error occurred: ', err);
+      });
+    axios.get('http://localhost:3000/colors')
+      .then(res => {
+        this.setState({
+          colors: res.data
+        });
       })
+      .catch(err => {
+        console.log('An error occurred: ', err);
+      });
+    axios.get('http://localhost:3000/sizes')
+      .then(res => {
+        this.setState({
+          sizes: res.data
+        });
+      })
+      .catch(err => {
+        console.log('An error occurred: ', err);
+      });
   }
 
   render() {
+    console.log(this.state.sizes[0].size);
     return (
-      <div>  
-        <h1>React is great!</h1>
-        {this.state.product.title}
+      <div className='main'>  
+        <div className='title'>
+          {this.state.product.title} 
+        </div>
+        <div className='brand'>
+          {this.state.product.brand}
+        </div>
+        <div className='price'>
+          <strong>{this.state.product.price}</strong><span className='tab'>Free Shipping</span>
+        </div>
+        <div className='description'>
+          {this.state.product.description}
+        </div>
+        <div className='sizes'>
+          <div>
+            <strong>Fit</strong>  <span className='small'>True to size.</span>
+          </div>
+          <select className='sizeDropdown'>
+            {this.state.sizes.map(size => <option className='sizeOption'>{size.size}</option>)}
+          </select>
+        </div>
+        <div className='colors'>
+          <select className='sizeDropdown'>
+            {this.state.colors.map(color => <option className='colorOption'>{color.color}</option>)}
+          </select>
+        </div>
       </div>
     );
   }
